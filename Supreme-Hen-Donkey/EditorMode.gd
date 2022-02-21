d8a4766e7e0a9d4d35b0fe0a806d5e4b95c5e2e0
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2022-02-20 21:33:56 -0500

    Drag and drop tiles

--- Supreme-Hen-Donkey/EditorMode.gd
+++ Supreme-Hen-Donkey/EditorMode.gd
@@ -1,24 +1,19 @@
 extends Node2D
 
-onready var tilemap = $TileMap
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export(NodePath) var tileMapNP: NodePath
+onready var tileMap = get_node(tileMapNP)
 
-onready var tileMap = $TileMap
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 
 func _input(event):
-	# Mouse in viewport coordinates.
-	if event.is_action_pressed("place_tile"):
-		var tilePos = tileMap.world_to_map(get_global_mouse_position())
-		tileMap.set_cellv(tilePos, 0)
-	
+#	if event.is_action_pressed("debug1"):
+#		placeTiles()
+	pass
 
-   # Print the size of the viewport.
-#   print("Viewport Resolution is: ", get_viewport_rect().size)
+
+func placeTiles():
+	for node in get_children():
+		node.placeTile()
--- Supreme-Hen-Donkey/FollowMouse.gd
+++ Supreme-Hen-Donkey/FollowMouse.gd
@@ -1,16 +1,45 @@
 extends Area2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export(NodePath) var tileMapNP: NodePath
+onready var tileMap = get_node(tileMapNP)
+onready var mouseOffset = $CollisionShape2D.shape.extents;
 
+var following: bool = false
+var startPos: Vector2
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	startPos = position
 
 
 func _physics_process(delta):
-	position = get_global_mouse_position()
+	if following:
+		followMouse()
+
+
+func _input_event(viewport, event, shape_idx):
+	# Start dragging
+	if event.is_action_pressed("click"):
+		if event.is_pressed():
+			following = true
+	# Release and put on tilemap
+	elif event.is_action_released("click"):
+		if following:
+			following = false
+#			placeTile()
+
+
+# Update position to snap to grid
+func followMouse():
+	position = tileMap.map_to_world(tileMap.world_to_map(get_global_mouse_position()))
+	position += mouseOffset
+
+
+# Places tile at current location
+func placeTile():
+	var tilePos = tileMap.world_to_map(position)
+	var tileType = 0
+	tileMap.set_cellv(tilePos, tileType)
 	
+	# DEBUG Reset tile to original position
+#	position = startPos
