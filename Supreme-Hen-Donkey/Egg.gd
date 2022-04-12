36090d77d09630c4e271eec6976745a803fdc30b
Ram Potham <ram.potham@gmail.com>
2022-04-12 15:28:17 -0400

    Update egg timer

--- Supreme-Hen-Donkey/Egg.gd
+++ Supreme-Hen-Donkey/Egg.gd
@@ -1,8 +1,10 @@
 extends KinematicBody2D
 export var gravity := 1200
+export var timer := 1
 
 var speed = 750
 var velocity = Vector2()
+var start_time = OS.get_ticks_msec()
 
 func start(pos, dir):
 	rotation = dir
@@ -17,6 +19,8 @@ func _physics_process(delta):
 		velocity = velocity.bounce(collision.normal)
 		if collision.collider.has_method("hit"):
 			collision.collider.hit()
+	if(OS.get_ticks_msec() - start_time > timer * 1000):
+		queue_free()
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
